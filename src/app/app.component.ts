6dd714836b4bfbe4aed00e0bc825cef90599fae6
Sami <sami7mason@yahoo.co.uk>
2017-08-18 19:40:56 +0100

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1> Sup modas! </h1>
-    <div *ngIf="myArr; then var1 else var2"></div>
-    <ng-template #var1>Silyllyly</ng-template>
-    <ng-template #var2>Silylsdfslyly</ng-template>
+    <h1> Sup Reshma! </h1>
+
+    <button [disabled]="buttonStatus">Buttton</button>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  myArr = true;
-
+buttonStatus = false
 }
