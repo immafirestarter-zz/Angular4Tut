581b5c2d387250577cbf165f163a89e02c7abf46
Sami <sami7mason@yahoo.co.uk>
2017-08-18 19:31:15 +0100

    lesson 3 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1> Sup modas! </h1>
+    <div *ngIf="myArr; then var1 else var2"></div>
+    <ng-template #var1>Silyllyly</ng-template>
+    <ng-template #var2>Silylsdfslyly</ng-template>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  myArr = true;
+
 }
